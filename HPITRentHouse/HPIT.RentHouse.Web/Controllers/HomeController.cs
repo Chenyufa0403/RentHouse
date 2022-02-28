6ecd25bba4bc1b104580aec451acc74fe2146bd1
Chenyufa <2910798432@qq.com>
2022-02-28 11:06:21 +0800

    加入前台文件夹

---
+++ HPITRentHouse/HPIT.RentHouse.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using HPIT.RentHouse.lService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPIT.RentHouse.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAdminUsersService _adminUsersService;
+        private readonly IHousesService _housesService;
+        public HomeController(IAdminUsersService adminUsersService, IHousesService housesService)
+        {
+            _adminUsersService = adminUsersService;
+            _housesService = housesService;
+        }
+        public ActionResult Index(int cityId = 0)
+        {
+            var list = _adminUsersService.CityList();
+            ViewBag.CityListName = list;
+            if (cityId > 0)
+            {
+                ViewBag.DefaultCity = list.Where(e => e.Id == cityId).FirstOrDefault().Name;
+            }
+            else
+            {
+                ViewBag.DefaultCity = list.FirstOrDefault().Name;
+            }
+            return View();
+        }
+    }
+}
