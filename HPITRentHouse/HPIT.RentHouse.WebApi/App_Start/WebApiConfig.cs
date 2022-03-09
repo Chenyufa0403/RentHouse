e6e4996a6b90f9acd8df2ae2c657e19236a89bca
Chenyufa <2910798432@qq.com>
2022-03-09 12:17:49 +0800

    API显示

--- HPITRentHouse/HPIT.RentHouse.WebApi/App_Start/WebApiConfig.cs
+++ HPITRentHouse/HPIT.RentHouse.WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace HPIT.RentHouse.WebApi
 {
@@ -10,7 +11,7 @@ namespace HPIT.RentHouse.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API 配置和服务
-
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
